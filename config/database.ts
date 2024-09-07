ccbfacca53fb5af48a598e54552cfc3adb4e2a5f
kumal3east <kumal3east@gmail.com>
2024-09-07 12:22:37 +0700

    editPackageJson

--- config/database.ts
+++ config/database.ts
@@ -2,17 +2,18 @@ import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
-  connection: 'postgres',
+  connection: env.get('DB_CONNECTION', 'postgres'),
   connections: {
     postgres: {
       client: 'pg',
-      connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
-      },
+      // connection: {
+      //   host: env.get('DB_HOST'),
+      //   port: env.get('DB_PORT'),
+      //   user: env.get('DB_USER'),
+      //   password: env.get('DB_PASSWORD'),
+      //   database: env.get('DB_DATABASE'),
+      // },
+      connection: env.get('DATABASE_URL'),
       migrations: {
         naturalSort: true,
         paths: ['database/migrations'],
